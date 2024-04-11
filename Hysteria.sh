17404330491628faa245fb3e11e6e96562db9074
passeway <164740303+passeway@users.noreply.github.com>
2024-04-11 10:11:19 +0800

    Update Hysteria.sh

--- Hysteria.sh
+++ Hysteria.sh
@@ -31,7 +31,7 @@ esac
 $PACKAGE_INSTALL unzip wget curl
 
 # 一键安装Hysteria2
-bash <(curl -fsSL https://get.hy2.sh/)
+/usr/bin/bash <(/usr/bin/curl -fsSL https://get.hy2.sh/)
 
 # 生成自签证书
 openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && sudo chown hysteria /etc/hysteria/server.key && sudo chown hysteria /etc/hysteria/server.crt
