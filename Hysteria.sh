efc3bcd3d0cfc71653d6fb5dc5cee1038d5ddd59
passeway <164740303+passeway@users.noreply.github.com>
2024-04-11 19:22:48 +0800

    Update Hysteria.sh

--- Hysteria.sh
+++ Hysteria.sh
@@ -10,9 +10,14 @@ uninstall_hysteria2() {
     bash <(curl -fsSL https://get.hy2.sh/) --remove
 }
 
+check_hysteria2_status() {
+    systemctl status hysteria-server.service
+}
+
 echo "请选择操作："
 echo "1. 安装 Hysteria2"
 echo "2. 卸载 Hysteria2"
+echo "3. 查看 Hysteria2 运行状态"
 read choice
 
 case $choice in
@@ -21,6 +26,9 @@ case $choice in
         ;;
     2)
         uninstall_hysteria2
+        ;;
+    3)
+        check_hysteria2_status
         ;;
     *)
         echo "无效的选项"
