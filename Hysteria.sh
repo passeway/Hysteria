6270a24d7e39f7e3edb6a42ca617212974d6ca44
passeway <164740303+passeway@users.noreply.github.com>
2024-04-11 10:06:16 +0800

    Update Hysteria.sh

--- Hysteria.sh
+++ Hysteria.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
 # 检查是否以 root 用户身份运行
-if [ "$EUID" -ne 0 ]
-  then echo "请以 root 用户身份运行此脚本"
-  exit
+if [ "$EUID" -ne 0 ]; then
+  echo "请以 root 用户身份运行此脚本"
+  exit 1
 fi
+
 # 判断系统及定义系统安装依赖方式
-REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora")
-RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora")
-PACKAGE_UPDATE=("apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update")
-PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "yum -y install")
-PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove" "yum -y remove")
-PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove")
+DISTRO=$(cat /etc/os-release | grep '^ID=' | awk -F '=' '{print $2}' | tr -d '"')
+case $DISTRO in
+  "debian"|"ubuntu")
+    PACKAGE_UPDATE="apt-get update"
+    PACKAGE_INSTALL="apt-get install -y"
+    PACKAGE_REMOVE="apt-get remove -y"
+    PACKAGE_UNINSTALL="apt-get autoremove -y"
+    ;;
+  "centos"|"fedora"|"rhel")
+    PACKAGE_UPDATE="yum -y update"
+    PACKAGE_INSTALL="yum -y install"
+    PACKAGE_REMOVE="yum -y remove"
+    PACKAGE_UNINSTALL="yum -y autoremove"
+    ;;
+  *)
+    echo "不支持的 Linux 发行版"
+    exit 1
+    ;;
+esac
 
 # 安装必要的软件包
-apt-get install -y unzip wget curl
+$PACKAGE_INSTALL unzip wget curl
 
 # 一键安装Hysteria2
 bash <(curl -fsSL https://get.hy2.sh/)
 
 # 生成自签证书
-openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=www.bing.com" -days 36500 && sudo chown hysteria /etc/hysteria/server.key && sudo chown hysteria /etc/hysteria/server.crt
+openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && sudo chown hysteria /etc/hysteria/server.key && sudo chown hysteria /etc/hysteria/server.crt
 
 # 随机生成端口和密码
-random_port=$(shuf -i 2000-65000 -n 1)
-random_password=$(openssl rand -base64 12)
+RANDOM_PORT=$(shuf -i 2000-65000 -n 1)
+RANDOM_PSK=$(openssl rand -base64 12)
 
 # 生成配置文件
 cat << EOF > /etc/hysteria/config.yaml
-listen: :$random_port # 监听随机端口
+listen: :$RANDOM_PORT # 监听随机端口
 
 # 使用自签证书
 tls:
@@ -37,12 +51,12 @@ tls:
 
 auth:
   type: password
-  password: "$random_password" # 设置随机密码
+  password: "$RANDOM_PSK" # 设置随机密码
   
 masquerade:
   type: proxy
   proxy:
-    url: https://www.bing.com # 伪装网址
+    url: https://bing.com # 伪装网址
     rewriteHost: true
 EOF
 
