6c758c0209781289c49f1bb1f91c72420fdaafee
passeway <164740303+passeway@users.noreply.github.com>
2024-10-02 13:46:47 +0800

    Update Hysteria2.sh

--- Hysteria2.sh
+++ Hysteria2.sh
@@ -36,9 +36,23 @@ bash <(curl -fsSL https://get.hy2.sh/)
 # 生成自签证书
 openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && sudo chown hysteria /etc/hysteria/server.key && sudo chown hysteria /etc/hysteria/server.crt
 
-# 随机生成端口和密码
-RANDOM_PORT=$(shuf -i 2000-65000 -n 1)
+# 生成随机端口号并检查端口是否被占用
+while true; do
+    RANDOM_PORT=$(shuf -i 50000-55000 -n 1)
+    if ! lsof -i:$RANDOM_PORT > /dev/null; then
+        echo "Selected port: $RANDOM_PORT"
+        break
+    else
+        echo "Port $RANDOM_PORT is in use, selecting a new one."
+    fi
+done
+
+# 生成随机密码
 RANDOM_PSK=$(openssl rand -base64 12)
+
+# 获取服务器证书的 SHA256 指纹
+SHA256=$(openssl x509 -in /etc/hysteria/server.crt -noout -fingerprint -sha256 | cut -d'=' -f2)
+echo $SHA256
 
 # 生成配置文件
 cat << EOF > /etc/hysteria/config.yaml
@@ -72,6 +86,18 @@ HOST_IP=$(curl -s http://checkip.amazonaws.com)
 # 获取IP所在国家
 IP_COUNTRY=$(curl -s http://ipinfo.io/${HOST_IP}/country)
 
+# 安装 iptables-persistent
+${PACKAGE_INSTALL} iptables-persistent
+
+# 端口跳跃设置：将50000-55000范围的UDP流量重定向到随机生成的端口
+iptables -t nat -A PREROUTING -p udp --dport 50000:55000 -j DNAT --to-destination :${RANDOM_PORT}
+ip6tables -t nat -A PREROUTING -p udp --dport 50000:55000 -j DNAT --to-destination :${RANDOM_PORT}
+
+# 保存 iptables 规则
+netfilter-persistent save
+
+
+
 # 输出客户端配置信息
 echo "Hysteria2 安装成功"
 cat << EOF
@@ -86,5 +112,7 @@ cat << EOF
   skip-cert-verify: true
   fast-open: true
 EOF
+echo
 echo "hy2://${RANDOM_PSK}@${HOST_IP}:${RANDOM_PORT}?insecure=1&sni=www.bing.com#${IP_COUNTRY}"
-echo "${IP_COUNTRY} = hysteria2, ${HOST_IP}, ${RANDOM_PORT}, password = ${RANDOM_PSK}, skip-cert-verify=true, sni=www.bing.com"
+echo
+echo "${IP_COUNTRY} = hysteria2, ${HOST_IP}, ${RANDOM_PORT}, password = ${RANDOM_PSK}, skip-cert-verify=true, sni=www.bing.com,  server-cert-fingerprint-sha256=${SHA256}, port-hopping=50000-55000, port-hopping-interval=30"
